f6a269d27a0a9c5c786b0679c6dd0c496ef78e39
Valerii <berezovskijvalera812@gmail.com>
2025-02-26 19:51:01 +0100

    add files

---
+++ cmd/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/GOLANG-NINJA/crud-audit-log/internal/config"
+	"github.com/GOLANG-NINJA/crud-audit-log/internal/repository"
+	"github.com/GOLANG-NINJA/crud-audit-log/internal/server"
+	"github.com/GOLANG-NINJA/crud-audit-log/internal/service"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	// Config initialization
+	cfg, err := config.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Context initialization
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Mongo connection
+	opts := options.Client()
+	opts.SetAuth(options.Credential{
+		Username: cfg.DB.Username,
+		Password: cfg.DB.Password,
+	})
+	opts.ApplyURI(cfg.DB.URI)
+
+	dbClient, err := mongo.Connect(ctx, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := dbClient.Ping(context.Background(), nil); err != nil {
+		log.Fatal(err)
+	}
+
+	// Database initialization
+	db := dbClient.Database(cfg.DB.Database)
+
+	//Repository, service, server initialization
+	auditRepo := repository.NewAudit(db)
+	auditService := service.NewAudit(auditRepo)
+	auditSrv := server.NewAuditServer(auditService)
+	srv := server.New(auditSrv)
+
+	fmt.Println("SERVER STARTED", time.Now())
+
+	// Server start
+	if err := srv.ListenAndServe(cfg.Server.Port); err != nil {
+		log.Fatal(err)
+	}
+}
